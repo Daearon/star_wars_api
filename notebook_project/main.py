5c0231c34ec2dfce9ec835934acc70cbd46d8ea2
Daearon <114180301+Daearon@users.noreply.github.com>
2023-03-31 19:33:43 +0300

    Delete notebook_project directory

--- notebook_project/main.py
+++
@@ -1,8 +0,0 @@
-from notebook import Notebook
-from controller import Controller
-from view import View
-
-notebook = Notebook()
-controller = Controller(notebook)
-view = View(controller)
-view.run()
