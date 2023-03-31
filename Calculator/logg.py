5650aeb6a747dedf9622a99c48423086ef9d9777
Daearon <114180301+Daearon@users.noreply.github.com>
2023-03-31 19:33:00 +0300

    Delete Calculator directory

--- Calculator/logg.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-
-logging.basicConfig(level=logging.INFO, filename="calculation_log.log",
-                    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s - %(message)s",
-                    datefmt='%d-%m-%Y %H:%M:%S')
--- Calculator/tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from mod_calc import *
-
-
-class TestCalculator(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(summation(0.25, 0.5), 0.75)
-
